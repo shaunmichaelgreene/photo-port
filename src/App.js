51f429a03a0a2676fa181eac944a3326a686af0d
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-09 19:11:02 -0400

    end 20.3.3 gallery component initialized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Nav />
       <main>
+          <Gallery />
           <About />        
       </main>
     </div>
--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,18 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-afterEach(cleanup);
-describe('About component', () => {
-    //first test
-    it('renders', () => {
-        render(<About />);
-    });
-    it('matches snapshot DOM node structure', () => {
-        //render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
 
-    //second test
+afterEach(cleanup);
+
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
