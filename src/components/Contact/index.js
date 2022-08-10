c00445af1f6970969c762a4947843d2ec77d3fc9
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-10 08:56:24 -0400

    end 20.4.5 input validation added to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,32 @@
 import React, { useState } from 'react'
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
-    
+    const [errorMessage, setErrorMessage] = useState('');
 //JSX
 
 function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
+    if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        console.log(isValid);
+        //isValid conditional statement
+        if (!isValid) {
+            setErrorMessage('Your email is invalid!');
+        } else {
+            setErrorMessage('');
+        }
+    } else {
+        if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required!`);
+        } else {
+            setErrorMessage('');
+        }
+    }
+    if(!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value });
+    }
 }
 
 function handleSubmit(e) {
